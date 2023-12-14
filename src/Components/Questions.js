076fb10630c4db7859d49e75d8e0c6dcadafb316
mohamed yassin <mohamedyas8295@gmail.com>
2023-12-14 21:45:21 +0200

    commit

--- src/Components/Questions.js
+++ src/Components/Questions.js
@@ -1,25 +1,6 @@
 import React, { useEffect, useState } from "react";
-<<<<<<< HEAD
 import { motion } from "framer-motion";
 import Timer from "./Timer";
-=======
-
-export default function Questions({ questions, index, answer, setStates }) {
-  const [seconds, setSeconds] = useState(59);
-  const [minuts, setminuts] = useState(3);
-
-  const finish = minuts < 0;
-
-  if (finish) {
-    setStates({ type: "timer", payload: "finish" });
-  }
-
-  useEffect(() => {
-    setInterval(() => {
-      setSeconds((s) => s - 1);
-    }, 1000);
-  }, [setSeconds]);
->>>>>>> 6d4e4aa3edb8162e8dc6b839cf2c988152699d97
 
 export default function Questions({ questions, index, answer, setStates }) {
   return (
@@ -61,30 +42,7 @@ export default function Questions({ questions, index, answer, setStates }) {
         <div
           className="d-flex col-12 justify-content-between"
           style={{ maxWidth: "500px" }}
-        >
-<<<<<<< HEAD
-          <Timer
-            questions={questions}
-            index={index}
-            answer={answer}
-            setStates={setStates}
-          ></Timer>
-=======
-          <button className="timer py-4 px-5">
-            0{seconds < 0 ? setminuts((m) => m - 1) : minuts}:
-            {seconds < 10 ? (seconds < 0 ? setSeconds(59) : "0") : null}
-            {seconds}
-          </button>
-          {answer || answer === 0 ? (
-            <button
-              onClick={(e) => setStates({ type: "next" })}
-              className="next py-4 px-5"
-            >
-              {index + 1 === questions.length ? "Finish" : "Next"}
-            </button>
-          ) : null}
->>>>>>> 6d4e4aa3edb8162e8dc6b839cf2c988152699d97
-        </div>
+        ></div>
       }
     </motion.div>
   );
