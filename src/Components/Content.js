bb0decded4cf5683478d80f20c2289cc17807307
mohamed yassin <mohamedyas8295@gmail.com>
2023-12-14 21:44:19 +0200

    commit

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,13 +1,3 @@
 export default function Content({ children }) {
-<<<<<<< HEAD
-  return (
-    <div >
-      {children}
-    </div>
-  );
-=======
   return <div>{children}</div>;
->>>>>>> 6d4e4aa3edb8162e8dc6b839cf2c988152699d97
 }
-
-
